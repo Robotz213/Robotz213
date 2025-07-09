ae0c61e7b60aba64d250cc7a422e48e847333bbf
Robotz213 <nicholas@robotz.dev>
2025-07-09 09:39:46 -0400

    feat: update PortugueseView with profile image and enhance card layout; improve CSS for responsiveness and dark mode

--- src/views/portuguese/PortugueseView.vue
+++ src/views/portuguese/PortugueseView.vue
@@ -3,6 +3,9 @@ import { navbarStore } from '@/store/navbar'
 import { storeToRefs } from 'pinia'
 import NavbarView from './components/NavbarView.vue'
 import AboutMeView from './components/views/AboutMeView.vue'
+
+import profile from '@/assets/img/108819185.jpeg'
+
 // import MyContactsView from './components/views/MyContactsView.vue'
 
 // import MyJourneyView from './components/views/MyJourneyView.vue'
@@ -30,7 +33,12 @@ function getComponent(page: string) {
     <div class="container-fluid overflow-y-auto mt-5" style="height: calc(100% - 57px)">
       <div id="content" class="row">
         <div class="col-3">
-          <div class="card" style="height: 100%"></div>
+          <div class="card p-5" style="height: 100%">
+            <div class="card-img d-flex flex-column align-items-center p-4 bg-body-tertiary">
+              <img class="clip-imagem" :src="profile" alt="" />
+            </div>
+            <div class="card-body bg-body-tertiary"></div>
+          </div>
         </div>
         <div class="col-7">
           <div class="card" style="height: calc(100dvh - 200px)">
@@ -62,5 +70,10 @@ function getComponent(page: string) {
 .slide-fade-leave-to {
   transform: translateX(20px);
   opacity: 0;
+}
+
+.clip-imagem {
+  max-width: 100%;
+  clip-path: rect(0% 95% 95% 5% round 20%);
 }
 </style>
